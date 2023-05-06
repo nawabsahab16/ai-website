f453499acb254f22951467c9058a5fba5689d46a
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-05-06 12:18:32 +0530

    created the body and added desc fro the website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,32 @@ const Hero = () => {
     justify-center items-center flex-col">
 
       <nav className="flex justify-between
-      items-center flex-col">
+      items-center w-full mb-10 pt-3">
 
-        
+        <img src={logo} alt="sumz_logo" 
+        className="w-28 object-contain"/>
 
+      
+       <button
+         type="button"
+         onClick={() => window.open('https://github.com/nawabsahab16/ai-website')}
+         className="black_btn"
+         >
+         Github 
+       </button>
 
       </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br
+        className="max-md:hidden"/>
+        <span className= "blue_gradient">OpenAI GPT-4 </span>
+
+      </h1>
+
+      <h2 className="desc">
+        Simplify your reading with Summize, an Open-Source article Summarizer that transforms lengthy articles into clear and concise summaries.
+      </h2>
 
     </header>
   )
