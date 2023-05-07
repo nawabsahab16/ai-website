31b7950b0e4faa86bdd8702c5139b4da6eea886d
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-05-07 12:03:22 +0530

    added

--- src/services/article.js
+++ src/services/article.js
@@ -4,18 +4,7 @@ import { createApi , fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const axios = require('axios');
 
-const options = {
-  method: 'GET',
-  url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
-  params: {
-    url: 'https://time.com/6266679/musk-ai-open-letter/',
-    length: '3'
-  },
-  headers: {
-    'X-RapidAPI-Key': 'aedff815f2mshff4e5da66ca3a11p1d4ce8jsna3fd75571a23',
-    'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-  }
-};
+
 
 try {
 	const response = await axios.request(options);
