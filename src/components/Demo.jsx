d902ecffc70065b52002c0084f29b16ea3140303
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-05-12 23:54:12 +0530

    added localstorage

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,10 +9,23 @@ const Demo = () => {
   const [article, setArticle] = useState({
     url: '',
     summary: '',
-  })
+  });
+
+  const [allArticles , setAllArticles ] = useState([]);
 
   const [getSummary, {error , isFetching }] = 
   useLazyGetSummaryQuery();
+
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    )
+
+    if(articlesFromLocalStorage){
+      setAllArticles(articlesFromLocalStorage)
+    }
+  },  []);
 
 
 
@@ -24,8 +37,12 @@ const Demo = () => {
     
     if(data?.summary) {
       const newArticle = { ...article, summary: data.summary };
+
+      const updatedAllArticles = [newArticle, ...allArticles];
     
     setArticle(newArticle);
+
+    setAllArticles(updatedAllArticles);
 
     console.log(newArticle);
      
